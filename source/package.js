3df0c4a924c38d550ff5ac2e833f4834a3066222
egfx <segrafix@gmail.com>
2013-09-24 00:48:39 -0700

    Update package.js

--- source/package.js
+++ source/package.js
@@ -1,30 +1,30 @@
 enyo.depends(
-	"$lib/layout",
-	"$lib/onyx",
-	"$lib/jsOAuth",
-    "$lib/webos-ext",
+	"https://rawgithub.com/minego/macaw-enyo/master/lib/layout",
+	"https://rawgithub.com/minego/macaw-enyo/master/lib/onyx",
+	"https://rawgithub.com/minego/macaw-enyo/master/lib/jsOAuth",
+    	"https://rawgithub.com/minego/macaw-enyo/master/lib/webos-ext",
 
-	"webos",
-	"BackGesture.js",
-	"Date.js",
-	"xhrimages.js",
+	"https://rawgithub.com/minego/macaw-enyo/master/webos",
+	"https://rawgithub.com/minego/macaw-enyo/master/BackGesture.js",
+	"https://rawgithub.com/minego/macaw-enyo/master/Date.js",
+	"https://rawgithub.com/minego/macaw-enyo/master/xhrimages.js",
 
-	"entities.js",
-	"twitter.js",
-	"adn.js",
+	"https://rawgithub.com/minego/macaw-enyo/master/entities.js",
+	"https://rawgithub.com/minego/macaw-enyo/master/twitter.js",
+	"https://rawgithub.com/minego/macaw-enyo/master/adn.js",
 
-	"toasters",
-	"compose",
-	"authorize",
-	"options",
-	"messages",
-	"profile",
-	"preview",
+	"https://rawgithub.com/minego/macaw-enyo/master/toasters",
+	"https://rawgithub.com/minego/macaw-enyo/master/compose",
+	"https://rawgithub.com/minego/macaw-enyo/master/authorize",
+	"https://rawgithub.com/minego/macaw-enyo/master/options",
+	"https://rawgithub.com/minego/macaw-enyo/master/messages",
+	"https://rawgithub.com/minego/macaw-enyo/master/profile",
+	"https://rawgithub.com/minego/macaw-enyo/master/preview",
 
-	"prefs.js",
-	"confirm.js",
-	"chooseaccount.js",
+	"https://rawgithub.com/minego/macaw-enyo/master/prefs.js",
+	"https://rawgithub.com/minego/macaw-enyo/master/confirm.js",
+	"https://rawgithub.com/minego/macaw-enyo/master/chooseaccount.js",
 
-	"macaw.css",
-	"main.js"
+	"https://rawgithub.com/minego/macaw-enyo/master/macaw.css",
+	"https://rawgithub.com/minego/macaw-enyo/master/main.js"
 );
